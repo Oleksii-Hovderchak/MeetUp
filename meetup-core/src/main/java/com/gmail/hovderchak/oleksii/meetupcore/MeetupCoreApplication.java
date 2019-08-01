9c766cc33fce46c5a2acac8f50bc93f9e800cf8f
Oleksii Hovderchak <ohovderchak@blackberry.com>
2019-08-01 16:50:15 +0300

    initial versions of core and ui are created

---
+++ meetup-core/src/main/java/com/gmail/hovderchak/oleksii/meetupcore/MeetupCoreApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.hovderchak.oleksii.meetupcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupCoreApplication.class, args);
+	}
+
+}
---
+++ meetup-core/src/test/java/com/gmail/hovderchak/oleksii/meetupcore/MeetupCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.hovderchak.oleksii.meetupcore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeetupCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
